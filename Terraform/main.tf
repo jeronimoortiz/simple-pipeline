b204200e13422132f26973dab9a42aa3f19a28f5
Norcal <jeronimoortizlr@gmail.com>
2024-02-21 03:23:51 -0300

    ....

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,11 +2,27 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "value"
+        version = "5.37.0"
     }
+  }
+  backend "s3" {
+    bucket = "simple-pipeline-backend"
+    key = "value"
+    region = "us-east-1"
+    
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_instance" "jenkins-instance" {
+  ami = "ami-0fe630eb857a6ec83"
+  instance_type = "t2.micro"
+  key_name = "ssh"
+  tags = {
+      Project = simple-pipeline
+      Origin = terraform
+  }
 }
